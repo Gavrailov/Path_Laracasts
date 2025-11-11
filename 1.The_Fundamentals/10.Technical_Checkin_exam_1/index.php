4a6b888790d099f859bbc3cce5e11ba43bb78038
Anton Gavrailov <reffa1691@gmail.com>
2025-11-11 14:49:20 +0000

    new chapter + exercises read

---
+++ 1.The_Fundamentals/10.Technical_Checkin_exam_1/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$business = [
+    'name' => 'Laracasts',
+    'cost' => 15
+];
+
+$business['name']; // Laracasts
+
+function register($user){
+
+    // Create the user record in the db.
+    // Log them in.
+    // Send welcome email.
+    // Redirect to their new dashboard.
+
+}
+
+require "index.view.php";
